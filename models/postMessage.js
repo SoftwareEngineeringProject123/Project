8ea12845a36726d1fcff4631eb8e5d8c52f040dc
SoftwareEngineeringProject123 <102318901+SoftwareEngineeringProject123@users.noreply.github.com>
2022-04-01 20:53:15 -0400

    Add files via upload

---
+++ models/postMessage.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+
+const postSchema = mongoose.Schema({ // how a post a structured
+
+    title : String, // title of the post
+    message : String, // description of the post
+    creator : String, // creator
+    tags : [String], // defining tags of the post
+    selectedFile : String, // image
+    likeCount : {
+
+        type : Number,
+        default : 0
+    },
+
+    createdAt : {
+
+        type : Date,
+        default : new Date()
+    }
+
+
+})
+
+
+const PostMessage = mongoose.model( 'PostMessage', postSchema );
+// mongoose.model( 'TABLE TO SAVE IT TO', THE SCHEMA NAME );
+
+
+export default PostMessage
+
+
