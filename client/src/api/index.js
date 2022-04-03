64a87512ddd55dbfb744722b795456f777a991aa
grahamt0 <gt431@mynsu.nova.edu>
2022-04-03 00:13:39 -0400

    finished Redux!, Form, Create a Post

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,8 +2,6 @@ import axios from 'axios'
 
 const url = `http://localhost:5000/posts` // route to get to the backend
 
+export const fetchPosts = () => axios.get(url); // return the posts in th database from the api/server
 
-export const fetchPosts = ()=>{
-
-    return axios.get(url) // return the posts in th database from the api/server
-}
+export const createPost = (newPost) => axios.post(url, newPost);
