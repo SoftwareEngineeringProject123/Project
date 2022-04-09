96fa014d9a0feb2532d94ad669bd6ab6939b6a51
grahamt0 <gt431@mynsu.nova.edu>
2022-04-09 15:06:10 -0400

    completed mobile responsiveness

--- client/src/App.js
+++ client/src/App.js
@@ -23,20 +23,20 @@ const App = ()=>{
 
     return (
 
-        <Container maxWidth={"lg"}>
+        <Container maxWidth="lg">
 
-            <AppBar className={classes.appBar} position={"static"} color={"inherit"}>
+            <AppBar className={classes.appBar} position="static" color="inherit">
 
-                <Typography className={classes.heading} variant={"h2"} align={"center"}> SNAPSHOTS </Typography>
-                <img className={classes.image}  src={memories} alt={'snapshots'} height={'60'}/>
+                <Typography className={classes.heading} variant="h2" align="center"> SNAPSHOTS </Typography>
+                <img className={classes.image}  src={memories} alt="snapshots" height="60"/>
 
             </AppBar>
             <Grow in>
 
                 <Container>
 
-                    <Grid container justify = "space-between" alighnItems={'stretch'} spacing={4}>
-
+                    <Grid className={classes.mainContainer} container justify="space-between"
+                    alignItems="stretch" spacing={3}>
                         <Grid item xs={12} sm={7}>
                             <Posts/>
                         </Grid>
