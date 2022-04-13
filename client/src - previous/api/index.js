697c8cefbb1873e9ff64c38030a62e5abdad52fa
SoftwareEngineeringProject123 <softwareengineerproject123@gmail.com>
2022-04-13 14:08:25 -0400

    new changes (color change(s))

--- client/src - previous/api/index.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-const url = `http://localhost:8000/posts` // route to get to the backend
-
-export const fetchPosts = () => axios.get(url); // return the posts in th database from the api/server
-
-export const createPost = (newPost) => axios.post(url, newPost);
-
-export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost);
